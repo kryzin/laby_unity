07e272635e2f28d0b64af2b9138beb141d148255
Karolina Ryzińska <76969276+kryzin@users.noreply.github.com>
2023-10-25 17:26:35 +0200

    zadanie4

--- Assets/Scripts/lab04/Zadanie4.cs
+++ Assets/Scripts/lab04/Zadanie4.cs
@@ -2,38 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveWithCharacter: MonoBehaviour
+public class MoveWithCharacterController : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    public float playerSpeed = 2.0f;
+    private float playerSpeed = 5.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
     private void Start()
     {
-        // zak³adamy, ¿e komponent CharacterController jest ju¿ podpiêty pod obiekt
         controller = GetComponent<CharacterController>();
     }
 
-    void Update()
+    // changed to FixedUpdate - isGrounded wasn't working properly in Update()
+    void FixedUpdate()
     {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
         groundedPlayer = controller.isGrounded;
+        Debug.Log("grounded? " + groundedPlayer);
+        // Debug logs true, but sometimes still unable to jump ??
+
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
 
-        // zmienia wysokoœæ postaci.
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
